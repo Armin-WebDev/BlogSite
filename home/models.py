ef6a2440c02b66e1415b14585a311e13450e73f7
ArminP <ArminPouralvani@outlook.com>
2022-12-02 19:37:06 +0330

    creating 3 models for blogs,users,blog categories

--- home/models.py
+++ home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to="avatar/" , null=False, blank=False)
+    description= models.TextField()
+
+class Post(models.Model):
+    title = models.CharField(max_length=127)
+    content = models.TextField()
+    image = models.ImageField(upload_to="blog/", null=False, blank=False)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=datetime.now, blank=False)
+    promote = models.BooleanField(default=False)
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image=models.ImageField(upload_to="category/", null=False, blank=False)
--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 
 def home_view(request):
+    
     return render(request,"index.html")
 
 def contact_view(request):
