f917f3557f71d71c0aa753946fdef8d3c78917be
ArminP <ArminPouralvani@outlook.com>
2022-12-06 21:36:52 +0330

    creating 3 more posts for home view and added caegoires view

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('',home_view,name="home"),
     path('contact/',contact_view,name="contact"),
     path('about/', about_view , name="about"),
+    path('categories/', categories_view , name="categories"),
+    #path('post-<int:pid>', categories_view , name="single"),
+
 ]
 
 urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def home_view(request):
-    
-    return render(request,"index.html")
+    posts = Post.objects.filter(promote=True)
+    context = {
+        "posts":posts
+    }
+    return render(request,"index.html",context)
 
 def contact_view(request):
     return render(request,"page-contact.html")
 
 def about_view(request):
     return render(request,"page-about.html")
+
+
+def categories_view(request):
+    return render(request,"category.html")
